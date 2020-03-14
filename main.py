8afb4430b9e7683880dd1175fe432d4d99cf9068
EinSteini <fanokopterhd@gmail.com>
2020-03-14 17:15:32 +0100

    Automated file structure for csv, default csv files to write into created

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import urllib.request
 import datetime
+import os
+from shutil import copyfile
+import csv
 
 urlToCheck = []
 vtrList = []
+basic = os.listdir("./data/basic/")
 
 # --Making a list with all forms in school--
 site = urllib.request.urlopen('https://gymnasium-remigianum.net/webuntis/frames/navbar.htm').read()
@@ -11,6 +15,7 @@ forms = (str(site).split('var classes = ["')[1]).split('"];')[0].split('","')
 # --Getting the current week dates--
 weeknumber = datetime.datetime.today().isocalendar()[1]
 weekday = datetime.datetime.today().weekday()
+date = str(datetime.datetime.today()).split('-')
 
 url = "https://gymnasium-remigianum.net/webuntis/"+str(weeknumber)+"/w/w00001.htm"
 src = urllib.request.urlopen(url).read()
@@ -24,7 +29,7 @@ try:
 
     for url in urlToCheck:
         src = urllib.request.urlopen(url).read()
-        currentDay = str(src).split('<a name="')[weekday + 1]
+        currentDay = str(src).split('<a name="')[weekday+1]
         vertretungen = currentDay.split(
             '>Klasse(n)</th><th class="list" align="center">Raum</th><th class="list" align="center">Text</th></tr>')[
             1].split("<tr class='list")
@@ -38,3 +43,21 @@ try:
 except IndexError:
     print("Die Vertretungen für diesen Tag sind noch nicht freigegeben")
 
+url = "https://www.nrw-ferien.de/nrw-ferien-"+date[0]+".html"
+holidayStart = str(urllib.request.urlopen(url).read()).split("title='Sommerferien 2020 in Nordrhein-Westfalen' hreflang='de-de'>Sommerferien</a></h3><p class='f_datum'>")[1].split("-")[0].split(" ")[1].split(".")
+
+if int(date[1]) <= int(holidayStart[1]) and int(date[2].split(' ')[0]) <= int(holidayStart[0]):
+    scndHalf = True
+else:
+    scndHalf = False
+
+if not os.path.exists("./data/" + str(int(date[0])-int(scndHalf)) + "_" + str(int(date[0])-int(scndHalf)+1)):
+    os.makedirs("./data/" + str(int(date[0])-int(scndHalf)) + "_" + str(int(date[0])-int(scndHalf)+1))
+    for file in basic:
+        copyfile("./data/basic/"+file, "./data/"+str(int(date[0])-int(scndHalf)) + "_" + str(int(date[0])-int(scndHalf)+1)+"/"+file)
+
+
+# with open('data.csv', encoding='utf8') as csv_file:
+#     data = csv.DictReader(csv_file)
+#     for dat in data:
+#         print(dat)
